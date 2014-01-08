ffb7b26c67e0f960b89aa21480ad879c327545ec
Shengkui Leng <lengshengkui@gmail.com>
2014-01-08 13:20:06 +0800

    Update e-mail in source file.

--- client.c
+++ client.c
@@ -7,7 +7,7 @@
 *     The example of client using Unix domain socket.
 *
 * REVISION(MM/DD/YYYY):
-*     12/02/2013  Shengkui Leng (shengkui.leng@gmail.com)
+*     12/02/2013  Shengkui Leng (lengshengkui@gmail.com)
 *     - Initial version 
 *
 ******************************************************************************/
